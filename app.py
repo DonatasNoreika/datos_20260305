a3d869085371067e8d7f7f1015e20311279b25e3
DonatasNoreika <donoras@gmail.com>
2026-03-05 10:40:22 +0200

    Pridėtas requirements.txt failas

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import datetime
+import pendulum
 
 data_str = input("Įveskite datą (YYYY-MM-DD): ")
-data = datetime.datetime.strptime(data_str, "%Y-%m-%d")
-delta = datetime.datetime.today() - data
+data = pendulum.from_format(data_str, "YYYY-MM-DD")
+delta = data.diff(pendulum.today())
 
-print(f"Nuo įvestos datos praėjo: {delta.days} dienų")
+print(f"Nuo įvestos datos praėjo: {delta.in_days()} dienų")
