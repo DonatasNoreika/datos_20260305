1c9c1373c40c45b0b575045357c1bfa0483272b2
DonatasNoreika <donoras@gmail.com>
2026-03-05 10:46:39 +0200

    Pridėtas papildomas kodas app.py faile

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ data_str = input("Įveskite datą (YYYY-MM-DD): ")
 data = pendulum.from_format(data_str, "YYYY-MM-DD")
 delta = data.diff(pendulum.today())
 
+print(f"Nuo įvestos datos praėjo: {delta.in_years()} metų")
 print(f"Nuo įvestos datos praėjo: {delta.in_days()} dienų")
